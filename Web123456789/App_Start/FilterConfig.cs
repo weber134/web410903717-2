d11369fa1e03ddf133c0f795390bf87b2c20cd05
vita <vita@test.com>
2023-04-26 19:07:58 +0800

    first commit

---
+++ Web123456789/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Web123456789
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Web123456789/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web123456789.MvcApplication" Language="C#" %>
