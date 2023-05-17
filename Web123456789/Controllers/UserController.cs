551e26dc57a22bb38208152071aa428b6af43fbd
vita <vita@test.com>
2023-05-17 20:30:42 +0800

    新增註冊

---
+++ Web123456789/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web123456789.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string name, string account, string password)
+        {
+            return View();
+        }
+    }
+}
