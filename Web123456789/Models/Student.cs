99a84e0a7bceebe338754864bf2666d5fd12c614
weber <weberandweber13@gmail.com>
2023-05-31 19:41:26 +0800

    5/31 one

--- Web123456789/Models/Student.cs
+++ Web123456789/Models/Student.cs
@@ -18,5 +18,6 @@ namespace Web123456789.Models
         public string Name { get; set; }
         public string Number { get; set; }
         public string Email { get; set; }
+        public Nullable<bool> IsActive { get; set; }
     }
 }
